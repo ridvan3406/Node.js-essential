4abde3a19bcdac3c77b1e530b3f92976aa428bd6
ridvan3406 <ridvansaman34@gmail.com>
2022-01-06 17:35:01 +0000

    rename and remove directories

---
+++ ch05/05_07/start/directories.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+fs.readdirSync("./storage").forEach(fileName =>{
+    fs.unlinkSync(`./storage/${fileName}`)
+})
+
+fs.rmdir("./storage", err =>{
+    if(err) throw err
+
+    console.log("./storage directory removed")
+})
