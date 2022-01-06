54400657fc918b8c2363462f5b9d32a0bbdcd280
ridvan3406 <ridvansaman34@gmail.com>
2022-01-06 18:10:26 +0000

    readable file streams

--- ch06/06_01/start/readStream.js
+++ ch06/06_01/start/readStream.js
@@ -1,4 +1,15 @@
-console.log("type something...");
-process.stdin.on("data", data => {
-  console.log(`I read ${data.length - 1} characters of text`);
+const fs = require("fs");
+
+const readStream = fs.createReadStream("./assets/lorum-ipsum.md", "UTF-8");
+
+let fileTxt =''
+
+readStream.on("data", data => {
+  console.log(`I read ${data.length - 1} of characters of text`);
+  fileTxt+= data
 });
+
+readStream.on("end", (end) => {
+  console.log('finished reading file')
+  console.log(fileTxt.length-1)
+})
